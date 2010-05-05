7e35d57e4339b4b2f04815b2f4bfc69b2967caca
Alan Gutierrez <alan@blogometer.com>
2010-05-04 23:06:56 -0500

    Updated Mix build for new project langauge.

--- src/mix/java/com/goodworkalan/ilk/io/IlkStreamProject.java
+++ src/mix/java/com/goodworkalan/ilk/io/IlkStreamProject.java
@@ -1,6 +1,5 @@
 package com.goodworkalan.ilk.io.mix;
 
-import com.goodworkalan.go.go.Artifact;
 import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
@@ -10,15 +9,15 @@ public class IlkStreamProject extends ProjectModule {
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces(new Artifact("com.github.bigeasy.ilk/ilk-stream/0.1"))
+                .produces("com.github.bigeasy.ilk/ilk-stream/0.1")
                 .main()
                     .depends()
-                        .artifact(new Artifact("com.github.bigeasy.ilk/ilk/0.1"))
+                        .include("com.github.bigeasy.ilk/ilk/0.+1")
                         .end()
                     .end()
                 .test()
                     .depends()
-                        .artifact(new Artifact("org.testng/testng/5.10/jdk15"))
+                        .include("org.testng/testng-jdk15/5.10")
                         .end()
                     .end()
                 .end()
