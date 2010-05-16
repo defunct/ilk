c1a243b2adbead3b43e872f5790ab939fa08d6de
Alan Gutierrez <alan@blogometer.com>
2010-05-16 18:44:21 -0500

    Mix ProjectModule is now an interface.

--- src/mix/java/com/goodworkalan/ilk/mix/IlkProject.java
+++ src/mix/java/com/goodworkalan/ilk/mix/IlkProject.java
@@ -4,8 +4,18 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class IlkProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Ilk.
+ *
+ * @author Alan Gutierrez
+ */
+public class IlkProject implements ProjectModule {
+    /**
+     * Build the project definition for Ilk.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
