cc08d49f3d2ee572da33e133e126b299b2493e4d
Alan Gutierrez <alan@blogometer.com>
2010-06-28 11:58:59 -0500

    Javadoc tidy.

--- src/main/java/com/goodworkalan/ilk/inject/ProviderInstanceVendor.java
+++ src/main/java/com/goodworkalan/ilk/inject/ProviderInstanceVendor.java
@@ -20,10 +20,15 @@ class ProviderInstanceVendor<I> extends Vendor<I> {
     
     /**
      * Create a provider instance vendor.
+     * 
      * @param ilk
+     *            The super type token of the instance to provide.
      * @param provider
+     *            The provider.
      * @param qualifier
+     *            The binding qualifier.
      * @param scope
+     *            The scope in which to store the constructed instance .
      */
     public ProviderInstanceVendor(Ilk<I> ilk, Provider<? extends I> provider, Class<? extends Annotation> qualifier, Class<? extends Annotation> scope) {
         super(ilk, qualifier, scope, null);
@@ -31,7 +36,12 @@ class ProviderInstanceVendor<I> extends Vendor<I> {
     }
 
     /**
+     * Get an unscoped boxed instance of the implementation provided by this
+     * vendor.
      * 
+     * @param injector
+     *            The injector.
+     * @return A boxed instance of the implementation.
      */
     public Ilk.Box get(Injector injector) {
         return ilk.box(provider.get());
--- src/main/java/com/goodworkalan/ilk/inject/ProviderVendor.java
+++ src/main/java/com/goodworkalan/ilk/inject/ProviderVendor.java
@@ -1,8 +1,5 @@
 package com.goodworkalan.ilk.inject;
 
-import static com.goodworkalan.ilk.Types.getRawClass;
-import static com.goodworkalan.ilk.inject.InjectException._;
-
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 
@@ -53,22 +50,5 @@ class ProviderVendor<I> extends Vendor<I> {
     public Ilk.Box get(Injector injector)
     throws InstantiationException, IllegalAccessException, InvocationTargetException {
         return ilk.box(injector.newInstance(reflector, provider.key).cast(provider).get());
-    }
-
-    /**
-     * Override the default provider implementation to return the provider
-     * instance specified by this provider vendor.
-     * 
-     * @param injector
-     *            The injector.
-     * @return A boxed provider instance.
-     */
-    @Override
-    Ilk.Box provider(Injector injector) {
-        try {
-            return injector.newInstance(reflector, provider.key);
-        } catch (Throwable e) {
-          throw new InjectException(_("Unable to create new instance of [%s].", e, getRawClass(provider.key.type)), e);
-        }
     }
 }
