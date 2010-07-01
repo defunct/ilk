f7ae971cb179b338f774575c0908d284f5d00bbc
Alan Gutierrez <alan@blogometer.com>
2010-07-01 00:16:15 -0500

    Created an AliasVendor.

---
+++ src/main/java/com/goodworkalan/ilk/inject/alias/AliasVendor.java
@@ -0,0 +1,55 @@
+package com.goodworkalan.ilk.inject.alias;
+
+import java.lang.annotation.Annotation;
+
+import com.goodworkalan.ilk.Ilk;
+import com.goodworkalan.ilk.Ilk.Box;
+import com.goodworkalan.ilk.inject.Injector;
+import com.goodworkalan.ilk.inject.Vendor;
+
+/**
+ * A vendor that aliases one type and qualifier with another type and qualifier.
+ * The alias type can be a super type of the aliased type or an implemented
+ * interface of the aliased type.
+ * 
+ * @author Alan Gutierrez
+ * 
+ * @param <T>
+ */
+public class AliasVendor<T> extends Vendor<T> {
+    /** The type to alias. */
+    private final Ilk<? extends T> fromIlk;
+    
+    /** The annotation for the type to alias. */
+    private final Class<? extends Annotation> fromQualifier;
+
+    /**
+     * Create an alias vendor.
+     * 
+     * @param ilk
+     *            The super type token of the binding.
+     * @param qualifier
+     *            The binding qualifier.
+     * @param fromIlk
+     *            The type to alias.
+     * @param fromQualifier
+     *            The annotation for the type to alias.
+     */
+    public AliasVendor(Ilk<T> ilk, Class<? extends Annotation> qualifier, Ilk<? extends T> fromIlk, Class<? extends Annotation> fromQualifier) {
+        super(ilk, qualifier, null, null);
+        this.fromIlk = fromIlk;
+        this.fromQualifier = fromQualifier;
+    }
+
+    /**
+     * Get an instance of the aliased type.
+     * 
+     * @param injector
+     *            The injector.
+     * @return The boxed aliased instance.
+     */
+    @Override
+    public Box get(Injector injector) {
+        return ilk.box(injector.instance(fromIlk, fromQualifier));
+    }
+}
---
+++ src/mix/java/com/goodworkalan/ilk/inject/alias/IlkInjectAliasProject.java
@@ -0,0 +1,30 @@
+package com.goodworkalan.ilk.mix;
+
+import com.goodworkalan.mix.ProjectModule;
+import com.goodworkalan.mix.builder.Builder;
+import com.goodworkalan.mix.cookbook.JavaProject;
+
+/**
+ * Builds the project definition for Ilk Inject Alias.
+ *
+ * @author Alan Gutierrez
+ */
+public class IlkInjectAliasProject implements ProjectModule {
+    /**
+     * Build the project definition Ilk Inject Alias.
+     *
+     * @param builder
+     *          The project builder.
+     */
+    public void build(Builder builder) {
+        builder
+            .cookbook(JavaProject.class)
+                .produces("com.github.bigeasy.ilk/ilk-inject-alias/0.1")
+                .depends()
+                    .production("com.github.bigeasy.ilk/ilk-inject/0.+1")
+                    .development("org.testng/testng-jdk15/5.10")
+                    .end()
+                .end()
+            .end();
+    }
+}
