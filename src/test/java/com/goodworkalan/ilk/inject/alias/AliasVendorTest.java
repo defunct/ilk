290cb7f797adb1a77e157eff8812687bdc2e5af5
Alan Gutierrez <alan@blogometer.com>
2010-07-01 13:02:59 -0500

    Created new tests.

---
+++ src/test/java/com/goodworkalan/ilk/inject/alias/AliasVendorTest.java
@@ -0,0 +1,15 @@
+package com.goodworkalan.ilk.inject.alias;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit tests for the {@link AliasVendor} class.
+ *
+ * @author Alan Gutierrez
+ */
+public class AliasVendorTest {
+    /** A test to get mix to build in continuous-integration. */
+    @Test
+    public void nothing() {
+    }
+}
